01f50fb2454a3a02da1c2cfb2b659db40d281c89
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-12 20:32:15 +0500

    practice set number 1

---
+++ practice_sets/practice_set1.js
@@ -0,0 +1,31 @@
+// let name = "Daniyal";
+// let num = 2;
+// let new1 = name + num;
+// console.log(new1);
+
+// console.log(typeof(new1));
+
+// const details = {
+//     name : "Daniyal",
+//     phone : "0312-3456789",
+//     address: "Nill"
+// }
+
+// details.phone = "0313-21122222"; // this is allowed
+// // details = 10; //this is not allowed
+// // details.phone = new "-";
+// console.log("Name: ",details.name);
+// console.log("Address: ",details.address);
+// console.log("Phone No: ",details.phone);
+
+const dict = {
+    a : "English alphabet",
+    apple : "Fruit",
+    bat : "an item used to play with / or an animal",
+    rude : "in state of being angry",
+    address: "location of a person / place or thing" 
+}
+console.log(dict.a);
+
+
+
