fb325e334d285e00f0efd20709753c3fdddbc780
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-13 11:50:11 +0500

    quizes set

---
+++ Quizes/quiz1.js
@@ -0,0 +1,9 @@
+const details = {
+    name : "Daniyal",
+    phone : "0312-3456789",
+    address: "Nill"
+}
+
+console.log("Name: ",details.name);
+console.log("Address: ",details.address);
+console.log("Phone No: ",details.phone);
