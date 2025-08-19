d2d05f6d2ff03122b09373a004cac2535c899723
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-19 20:35:30 +0500

    added practice set 4

---
+++ practice_sets/practice_set4.js
@@ -0,0 +1,20 @@
+// console.log("har\"".length); // it will print the length of inside words
+// let name = "Daniyal";
+// console.log(name.length);
+/*
+let item = "This is an item and it contain sugar";
+
+console.log(item.includes("sugar1"));
+console.log(item.endsWith("sugar"));
+console.log(item.startsWith("this")); // case sensitive this -> not equal to This, will return false
+*/
+
+// let str = "THIS A UPPER CASE LETTER";
+// console.log(str.toLowerCase());
+
+let take = "pls give RS 100";
+console.log(take.slice(12));
+console.log(take);
+// let name = "daniyal";
+// name[4] = 'd'   // string are immutable
+// console.log(name);
