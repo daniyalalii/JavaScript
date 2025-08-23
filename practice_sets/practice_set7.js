c990f0bf5f55ccc121aa921818dd7df5c0dd0a02
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-23 18:38:20 +0500

    practice set 7 added

---
+++ practice_sets/practice_set7.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".navbar li").forEach(e=>{
+    e.style.backgroundColor = "cyan";
+});
+// let firstItem = navbar.firstElementChild;
+// firstItem.style.backgroundColor = "yellow";
+// let lastItem = navbar.lastElementChild;
+// lastItem.style.backgroundColor = "green";
+
+// // navbar.style.backgroundColor = "cyan";
+// let items = navbar.getElementsByTagName("li");
